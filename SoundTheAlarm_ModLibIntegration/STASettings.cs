45f98d6a5bd5bb324792e0b98e8c9e6b44adb5ff
Jirow13 <67016716+Jirow13@users.noreply.github.com>
2020-09-02 12:45:11 -0400

    Convert to ButterLib-MCM4

--- SoundTheAlarm_ModLibIntegration/STASettings.cs
+++ SoundTheAlarm_ModLibIntegration/STASettings.cs
@@ -1,17 +1,27 @@
-﻿using ModLib.Definitions;
-using ModLib.Definitions.Attributes;
+﻿using HarmonyLib;
+using Bannerlord.ButterLib;
+using MCM.Abstractions.Attributes.v1;
+using MCM.Abstractions.Attributes.v2;
+using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Settings.Base.Global;
+using MCM.Utils;
 using System.Xml.Serialization;
+using System.Runtime.CompilerServices;
 
 namespace SoundTheAlarm
 {
-    public class STASettings : SettingsBase
+    //public class STASettings : BaseSettings
+    public class STASettings : AttributeGlobalSettings<STASettings>
     {
         public const string InstanceID = "SoundTheAlarmSettings";
-        public override string ModName => "Sound The Alarm";
-        public override string ModuleFolderName => STAMain.ModuleFolderName;
+        public override string DisplayName => "Sound The Alarm";
+        public override string FolderName => STAMain.ModuleFolderName;
         [XmlElement]
-        public override string ID { get; set; } = InstanceID;
+        public override string Id { get; } = InstanceID;
 
+
+        //public static STASettings Instance = new STASettings();
+        /*
         public static STASettings Instance
         {
             get
@@ -19,49 +29,51 @@ namespace SoundTheAlarm
                 return (STASettings)SettingsDatabase.GetSettings<STASettings>();
             }
         }
+       */
+
 
         [XmlElement]
-        [SettingProperty("Enable Village Popup", "Enables popups when your villages are attacked.")]
+        [SettingPropertyBool("Enable Village Popup", Order = 1, RequireRestart = false, HintText = "Enables popups when your villages are attacked.")]
         [SettingPropertyGroup("1. Fiefs")]
         public bool EnableVillagePopup { get; set; } = true;
 
         [XmlElement]
-        [SettingProperty("Enable Castle Popup", "Enables popups when your castles are attacked.")]
+        [SettingPropertyBool("Enable Castle Popup", Order = 2, RequireRestart = false, HintText = "Enables popups when your castles are attacked.")]
         [SettingPropertyGroup("1. Fiefs")]
         public bool EnableCastlePopup { get; set; } = true;
 
         [XmlElement]
-        [SettingProperty("Enable Town Popup", "Enables popups when your towns are attacked.")]
+        [SettingPropertyBool("Enable Town Popup", Order = 3, RequireRestart = false, HintText = "Enables popups when your towns are attacked.")]
         [SettingPropertyGroup("1. Fiefs")]
         public bool EnableTownPopup { get; set; } = true;
 
         [XmlElement]
-        [SettingProperty("Enable Declaration of War Popup", "Enables popups when factions declare war on one another.")]
+        [SettingPropertyBool("Enable Declaration of War Popup", Order = 4, RequireRestart = false, HintText = "Enables popups when factions declare war on one another.")]
         [SettingPropertyGroup("2. Declarations")]
         public bool EnableWarPopup { get; set; } = true;
 
         [XmlElement]
-        [SettingProperty("Enable Declaration of Peace Popup", "Enables popups when factions declare peace with one another.")]
+        [SettingPropertyBool("Enable Declaration of Peace Popup", Order = 5, RequireRestart = false, HintText = "Enables popups when factions declare peace with one another.")]
         [SettingPropertyGroup("2. Declarations")]
         public bool EnablePeacePopup { get; set; } = true;
 
         [XmlElement]
-        [SettingProperty("Enable Minor Faction Popups", "Enables popups for declarations of war/peace for minor factions.")]
+        [SettingPropertyBool("Enable Minor Faction Popups", Order = 6, RequireRestart = false, HintText = "Enables popups for declarations of war/peace for minor factions.")]
         [SettingPropertyGroup("2. Declarations")]
         public bool EnableMinorFactionPopup { get; set; } = true;
 
         [XmlElement]
-        [SettingProperty("Enable Pause Game on Popup", "Enables the game to be paused when popups are shown.")]
+        [SettingPropertyBool("Enable Pause Game on Popup", Order = 7, RequireRestart = false, HintText = "Enables the game to be paused when popups are shown.")]
         [SettingPropertyGroup("3. Miscellaneous")]
         public bool PauseGameOnPopup { get; set; } = true;
 
         [XmlElement]
-        [SettingProperty("Time to Remove Village From List", 0.0f, 20.0f, "Sets the time to remove the village from the managed settlements list.")]
+        [SettingPropertyFloatingInteger("Time to Remove Village From List", 0.0f, 20.0f, Order = 8, HintText = "Sets the time to remove the village from the managed settlements list.")]
         [SettingPropertyGroup("3. Miscellaneous")]
         public float TimeToRemoveVillageFromList { get; set; } = 10.0f;
 
         [XmlElement]
-        [SettingProperty("Enable Debug Messages", "Enables debug messages.")]
+        [SettingPropertyBool("Enable Debug Messages", Order = 7, RequireRestart = false, HintText = "Enables debug messages.")]
         [SettingPropertyGroup("4. Debug")]
         public bool EnableDebugMessages { get; set; } = false;
     }
